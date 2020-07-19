40376b519db9afdb29327ffb686d81629d814ebe
roj88 <rjc@gmail.com>
2020-07-19 02:10:29 -0700

    add new updates to abtest package

---
+++ csc-470d/final-project/abtest/args.py
@@ -0,0 +1,8 @@
+import os
+import sys
+import argparse
+
+class Parser:
+	
+
+	parser = argparse.ArgumentParser()
--- csc-470d/final-project/abtest/bootstrap.py
+++ csc-470d/final-project/abtest/bootstrap.py
@@ -1,17 +1,20 @@
 class Bootstrap:
-  
+  def __init__(self, dataframe):
+        self.dataframe = dataframe
+        self.bernoulli = bernoulli
+        self.num_datasets = num_datasets
+        self.control_treatment = control_treatment
+        self.num_samples = num_samples
 
 
-def bootstrap_monte_carlo_numeric(num_datasets, dataframe, pre_post, metric, num_samples=dataframe.size):
-  """Performs bootstrap monte carlo.
-
-  Performs bootstrap monte carlo on data from the table
-  rjcarter.duet_analysis_pre_post
+def bootstrap_monte_carlo(bernoulli, num_datasets, dataframe, control_treatment, num_samples=dataframe.size):
+  """Performs bootstrap monte carlo on a pandas dataframe
 
   Args:
+    bernoulli (bool): Whether the input data is a bernoulli or numeric output
     num_datasets (int): Number of aggregations to perform.
-    dataframe (Panda Dataframe): Data frame with pre_post and metric totals.
-    pre_post (String; 'Pre' or 'Post'): Pre-period vs post-period.
+    dataframe (Panda Dataframe): Data frame with metrics.
+    control_treatment (String; 'Treatment' or 'Control'): Treatment vs Control.
     metric (float): Metric name from dataframe.
     Optional: num_samples (int): Number of samples in each avg. Default is
                                 dataframe.size.
@@ -23,7 +26,7 @@ def bootstrap_monte_carlo_numeric(num_datasets, dataframe, pre_post, metric, num
   # array that will hold correlated data
   bootstrap_data_sets = []
 
-  df = dataframe.loc[dataframe['pre_post']==pre_post]
+  df = dataframe.loc[dataframe['control_treatment']==control_treatment]
   df.reset_index(drop=True, inplace=True)
 
   # gets the size of the data set to prevent out of bounds errors
@@ -37,19 +40,33 @@ def bootstrap_monte_carlo_numeric(num_datasets, dataframe, pre_post, metric, num
     # print out status message
     if i%increments==0:
       print("{}% done".format(100*i/num_datasets))
+    
     # define search and omnibox_counts
-    total = 0
+    metric_total = 0
+    successes_total = 0
+    failures_total = 0
 
     # iterate through the number of samples
     for j in range(num_samples):
       # get random row numbers
       row_num = r.randint(0, data_frame_size - 1)
 
-      # append new samples to the data set
-      total += df[metric].loc[row_num]
+      if bernoulli:
+        # append new samples to the data set
+        successes_total += df['successes'].loc[row_num]
+        trials_total += df['trials'].loc[row_num]
 
-    # append boostrapped data sets
-    bootstrap_data_sets.append(np.true_divide(total, num_samples))
+      else:
+        # append new samples to the data set
+        metric_total += df['metric'].loc[row_num]
+
+    if bernoulli:
+      # append boostrapped data sets
+      bootstrap_data_sets.append(np.true_divide(successes_total, trials_total))
+    else:
+      # append boostrapped data sets
+      bootstrap_data_sets.append(np.true_divide(metric_total, num_samples))
+
 
   print('Progress Complete!')
 
